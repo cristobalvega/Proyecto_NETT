603d2cd7d22a7a01a10b108ffba00545fab1e94a
Cristobal Vega <cristobalvega96@gmail.com>
2017-01-17 18:43:56 -0700

    Primeras lineas de código

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Proyecto NETT</title>
+		<meta name="description" content="Videojuego Web">
+		<meta name="author" content="Cristobal Vega">
+	</head>
+	<body>
+			<h1>Proyecto NETT</h1>
+			<h3>No Es TecTime-out</h3>
+	</body>
+</html>
