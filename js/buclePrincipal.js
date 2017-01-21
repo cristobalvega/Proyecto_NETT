038723a10e9b34b9bb9a9d14f603c8d28fc033fd
Cristobal Vega <cristobalvega96@gmail.com>
2017-01-21 16:20:04 -0700

    "Estructura del bucle principal"

---
+++ js/buclePrincipal.js
@@ -0,0 +1,24 @@
+//Namespace = Espacio de nombres 
+//main loop = Bucle principal
+//aps = Actualizaciones por segundo
+//fps = Frames ó Cuadros por segundo 
+//callback
+
+var buclePrincipal = {
+		idEjecucion: null,
+		ultimoRegistro: 0,
+		aps: 0,
+		fps: 0,
+		iterar: function(){
+
+		}
+		detener: function(){
+
+		}
+		actualizar: function(){
+
+		}
+		dibujar: function(){
+
+		}
+};
