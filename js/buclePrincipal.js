72f816b80edf6cd60a659bed21dd5c2d7f3060e3
cristobalvega96 <cristobalvega96@gmail.com>
2017-01-26 19:53:33 -0700

    "Se agregó un contador FPS"

--- js/buclePrincipal.js
+++ js/buclePrincipal.js
@@ -1,7 +1,7 @@
-//Namespace = Espacio de nombres 
+//Namespace = Espacio de nombres
 //main loop = Bucle principal
 //aps = Actualizaciones por segundo
-//fps = Frames ó Cuadros por segundo 
+//fps = Frames ó Cuadros por segundo
 //callback
 
 var buclePrincipal = {
@@ -10,15 +10,27 @@ var buclePrincipal = {
 		aps: 0,
 		fps: 0,
 		iterar: function(registroTemporal){
-			buclePrincipal.idEjecucion = window.requestAnimationFrame(buclePrincipal.iterar); //<---RECURSIVIDAD
+			buclePrincipal.idEjecucion = window.requestAnimationFrame(buclePrincipal.iterar); //<---RECURSIVO
+
+			//registroTemporal es un valor, el tiempo / milísegundos
+
+			buclePrincipal.actualizar(registroTemporal); //<-A registroTemporal lo llama el requestAnimationFrame
+			buclePrincipal.dibujar(registroTemporal);
+
+			if(registroTemporal - buclePrincipal.ultimoRegistro > 999){
+				buclePrincipal.ultimoRegistro = registroTemporal;
+				console.log("APS: " + buclePrincipal.aps + " | FPS : " buclePrincipal.fps);
+				buclePrincipal.aps=0;
+				buclePrincipal.fps=0;
+			}//If
 		},//Iterar
 		detener: function(){
 
 		},//Detener
 		actualizar: function(registroTemporal){
-
+			buclePrincipal.aps++;
 		},//Actualizar
 		dibujar: function(registroTemporal){
-
+			buclePrincipal.fps++;
 		}//Dibujar
 };
